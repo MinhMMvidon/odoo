e0862f127c879425a69a0f1a3be4f588f9a97eb7
minhtech <ctyminhtech@gmail.com>
2023-08-23 20:15:53 -0700

    just commit

---
+++ odooSandbox/estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Author Name",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+
+    'installable': True,
+}
