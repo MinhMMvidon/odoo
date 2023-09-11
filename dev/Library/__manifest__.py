baa75b63b8af52d9d36af32e04388d17baa2dd54
minhtech <ctyminhtech@gmail.com>
2023-09-11 14:19:08 +0700

    just commit

---
+++ dev/Library/__manifest__.py
@@ -0,0 +1,23 @@
+{
+
+    "name": "library_app",
+    "installable": True,
+
+    "summary": "Manage library catalog and book lending.",
+
+    "author": "Daniel Reis",
+
+    "license": "AGPL-3",
+
+    "website": "https://github.com/PacktPublishing"
+
+               "/Odoo-15-Development-Essentials",
+
+
+    "version": "15.0.1.0.0",
+
+    "depends": ["base"],
+
+    "application": True,
+
+}
